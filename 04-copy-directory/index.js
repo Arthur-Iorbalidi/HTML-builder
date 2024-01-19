c868d899205c4ba41af9652050d80448f3a7e353
Arthur Iorbalidi <arturiorbalidi01052005@gmail.com>
2024-01-19 18:23:19 +0300

    feat: solve the task 4

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,26 @@
+function copyDir() {
+    const fs  = require('fs');
+    const path = require('path');
+    const copyFolderPath = path.join(__dirname, 'files-copy');
+    const sourseFolderPath = path.join(__dirname, 'files');
+    fs.mkdir(copyFolderPath, { recursive: true }, (error) => {
+        if (error) {
+            console.log(error);
+        }
+    });
+    fs.readdir(sourseFolderPath, (error, files) => {
+        if (error) {
+            console.log(error);
+        }
+        else {
+            files.forEach((file) => {
+                fs.copyFile(path.join(sourseFolderPath, file), path.join(copyFolderPath, file), (error) => {
+                    if (error) {
+                        console.log(error);
+                    }
+                })
+            });
+        }
+    });
+}
+copyDir();
