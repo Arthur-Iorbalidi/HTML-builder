deb02b3d8673609bdcb2d8128801720c2cafb73f
Arthur Iorbalidi <arturiorbalidi01052005@gmail.com>
2024-01-21 13:43:32 +0300

    feat: change the task 5, delete all sync functions

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,9 +1,29 @@
 const fs = require('fs');
 const path = require('path');
 const sourseFolderPath = path.join(__dirname, 'styles');
-const cssFileNames = fs.readdirSync(sourseFolderPath).filter((file) => path.extname(file) === '.css');
-async function CreateBundle() {
-    fs.writeFileSync(path.join(__dirname, 'project-dist/bundle.css'), '');
+async function getCssFiles() {
+    return new Promise((resolve, reject) => {
+        fs.readdir(sourseFolderPath, (error, files) => {
+            if (error) {
+                reject(error);
+            }
+            else {
+                resolve(files.filter((file) => path.extname(file) === '.css'))
+            }
+        });
+    });
+}
+async function CreateBundle(cssFileNames) {
+    await new Promise((resolve) => {
+        fs.writeFile(path.join(__dirname, 'project-dist/bundle.css'), '', (error) => {
+            if (error) {
+                console.log(error);
+            }
+            else {
+                resolve();
+            }
+        });
+    });
     for (const file of cssFileNames) {
         const readStream = fs.createReadStream(path.join(sourseFolderPath, file), 'utf8');
         const writeStream = fs.createWriteStream(path.join(__dirname, 'project-dist/bundle.css'), { flags: 'a'});
@@ -18,4 +38,8 @@ async function CreateBundle() {
         });
     };
 }
-CreateBundle();
+async function MergeStyles() {
+    const cssFileNames = await getCssFiles();
+    CreateBundle(cssFileNames);
+}
+MergeStyles();
